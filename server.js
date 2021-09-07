79245c77537f531f9b9cc5b90053d30397e48318
Pulkit007 <77740386+Pulkit007@users.noreply.github.com>
2021-09-07 13:55:39 +0530

    feat: database initialize and connected

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
